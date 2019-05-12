1fb9a32e19a83e25d5d9526f3336a8aea2989204
Franco <franco_vega@msn.com>
2019-05-12 14:10:53 -0300

    Finished basic Inventory system, can save and load inventory

---
+++ Unity-Inventory/Assets/Scripts/Gameplay/InventoryList.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class InventoryList
+{
+    public List<ScriptableObjectItem> Inventory;
+
+    public string Serialize()
+    {
+        return JsonUtility.ToJson(this);
+    }
+
+    public void Deserialize(string json)
+    {
+        JsonUtility.FromJsonOverwrite(json, this);
+    }
+}
---
+++ Unity-Inventory/Assets/Scripts/Utils/ScriptableObjectItem.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+[CreateAssetMenu(fileName = "ScriptableObjectItem", menuName = "Frank/Utils/ScriptableObjectItem" + "", order = 1)]
+public class ScriptableObjectItem : ScriptableObject
+{
+    //https://docs.unity3d.com/Manual/class-ScriptableObject.html
+    public string itemName;
+    public string type;
+    public float damage;
+    public float weight;
+    public int id;
+    //public Sprite imagen;
+}
